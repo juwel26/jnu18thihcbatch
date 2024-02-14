a0fd54265cd811e42bdda1dd97f4ba015678528f
MD. Juwel Rana <rk370613@gmail.com>
2024-02-14 14:05:25 +0600

    ok

--- js/gallery.js
+++ js/gallery.js
@@ -1,28 +1,55 @@
 
-// async function logMovies() {
-//     const response = await fetch("https://juwel26.github.io/jnu18thihc/studentApi.json");
-//     const movies = await response.json();
-//     const realDAta = movies.students ;
-//     let  show = document.querySelector(".show")
-//     show.innerHTML = `${realDAta.map(function(students){
-//       return`
-
-//       <div class="show h-auto w-full bg-slate-100 border-2 border-orange-400 mt-2 ">
-      
-    
-//       <img src="${students.img}"  alt="" class="rounded-full w-20 h-20" />
-//       <h1 class="text-2xl font-bold text-slate-900  "> Name : ${students.name}</h1>
-//       <p class="font-semibold text-lg text-indigo-500"> ID Number: ${students.id} </p>
-//       <p class="font-semibold text-lg text-gray-500"> Dept: ${students.Dept} </p>
-//       <p class="font-semibold text-lg text-gray-500"> session: ${students.session} </p>
-//       <p class="font-semibold text-lg text-gray-500"> Number: ${students.number} </p>
-//       <a class="font-semibold text-lg text-indigo-500 underline" href="${students.facebook}">contact by facebook</a>
-      
-//        </div> `
-
-//     }).join(" ")}`
+async function logMovies() {
+    const response = await fetch("https://juwel26.github.io/jnu18thihc/studentApi.json");
+    const movies = await response.json();
+    const realDAta = movies.students ;
+    let  show = document.querySelector(".show")
+     console.log(realDAta)
+    show.innerHTML = `${realDAta.map(function(students){
+      return`
 
 
-//   }
+      <div class="max-w-xs m-2 justify-center items-center mx-auto  ">
+       <div class="bg-white shadow-xl rounded-lg py-3">
+           <div class="photo-wrapper p-2">
+               <img class="w-24 h-24 rounded-full mx-auto ring-2 shadow-2xl ring-orange-500" src="${students.img}" alt="${students.name}">
+           </div>
+           <div class="p-2 ">
+               <h3 class="text-center text-xl text-gray-900 font-medium leading-8 "> ${students.name}</h3>
+               <div class="text-center text-gray-400 text-xs font-semibold">
+                   <p class="text-indigo-500">${students.id}</p>
+               </div>
+               <table class="text-xs my-3 font-semibold">
+                   <tbody><tr>
+                       <td class="px-2 py-2 text-gray-500 font-semibold">Email :</td>
+                       <td class="px-2 py-2">${students.email}</td>
+                   </tr>
+                   <tr>
+                       <td class="px-2 py-2 text-gray-500 font-semibold">Phone :</td>
+                       <td class="px-2 py-2">${students.number}</td>
+                   </tr>
+                   <tr>
+                   <td class="px-2 py-2 text-gray-500 font-semibold">Batch :</td>
+                   <td class="px-2 py-2">${students.Batch}</td>
+               </tr>
+                   <tr>
+                       <td class="px-2 py-2 text-gray-500 font-semibold">session :</td>
+                       <td class="px-2 py-2">${students.session}</td>
+                   </tr>
+               </tbody></table>
+                <hr>
+               <div class="text-center my-3">
+                   <a class="text-xs text-indigo-500 italic bg-indigo-100 p-2 rounded-md pa hover:underline hover:text-indigo-600 font-medium" href="${students.facebook}">Facebook Profile</a>
+               </div>
+   
+           </div>
+       </div>
+   </div>  `
+   
+   
+    }).join(" ")}`
 
-//    logMovies()
+
+  }
+
+   logMovies()
