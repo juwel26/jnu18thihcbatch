278cb6548f05bfdedeae714cb6366a1ef64f2e86
MD. Juwel Rana <rk370613@gmail.com>
2024-02-14 10:35:30 +0600

    data upload

---
+++ js/gallery.js
@@ -0,0 +1,35 @@
+
+async function logMovies() {
+    const response = await fetch("https://juwel26.github.io/jnu18thihc/studentApi.json");
+    const movies = await response.json();
+    const realDAta = movies.students ;
+    let  show = document.querySelector(".show")
+    show.innerHTML = `${realDAta.map(function(students){
+      return`
+
+      <div class="show h-auto w-full bg-slate-100 border-2 border-orange-400 mt-2 ">
+      <h1 class="text-2xl font-bold text-slate-900  "> Name : ${students.name}</h1>
+      <p class="font-semibold text-lg text-indigo-500"> ID Number: ${students.id} </p>
+      <p class="font-semibold text-lg text-gray-500"> Dept: ${students.Dept} </p>
+      <p class="font-semibold text-lg text-gray-500"> session: ${students.session} </p>
+      <p class="font-semibold text-lg text-gray-500"> Number: ${students.number} </p>
+      <a class="font-semibold text-lg text-indigo-500 underline" href="${students.facebook}">contact by facebook</a>
+      
+       </div>
+    
+      `
+    
+   
+    }).join(" ")}`
+
+
+
+
+    // realDAta.map((students)=>{
+    //   console.log(students.name)
+    //  })
+    console.log(realDAta)
+
+  }
+
+   logMovies()
