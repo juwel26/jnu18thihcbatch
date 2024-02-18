8eb17b9f86224dcdd49b850d9574b98eeb900fc9
MD. Juwel Rana <rk370613@gmail.com>
2024-02-18 21:48:52 +0600

    ok

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+// for any timer update ***********************
 
 // Set the date we're counting down to
 var countDownDate = new Date("mar 3, 2024 8:00:00").getTime();
@@ -35,5 +36,40 @@ var x = setInterval(function() {
   }
 }, 1000);
 
-let demo = 
-console.log(demo)
+
+
+
+// for marque **************
+
+function marque(){
+  // Set the date we're counting down to
+var countDownDate = new Date("mar 1, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+   document.querySelector(".paragrap").innerHTML = "    🔥 🔥 📰 1st mid exam of 2nd semester will be start 03/03/2024. 🔥 ";
+    
+  }
+}, 1000);
+
+return x
+
+}
+
+marque();
