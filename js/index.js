cb5ad060252644d415042d030ba166191545c049
MD. Juwel Rana <rk370613@gmail.com>
2024-03-23 21:39:11 +0600

    done

--- js/index.js
+++ js/index.js
@@ -79,14 +79,11 @@ function showtime() {
   const Time = date.getHours();
 
   function counter() {
-    let date = new Date().getDate();
-    let month = new Date().getMonth();
-    let year = new Date().getFullYear();
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
     let seconds = new Date().getSeconds();
 
-    console.log(timerShow);
+    // console.log(timerShow);
 
     if (Time >= 0 && Time < 12) {
       timerShow.innerHTML = `🌤️Good Morning ${hours}: ${minutes}: ${seconds}`;
@@ -101,3 +98,68 @@ function showtime() {
 }
 
 showtime();
+
+// wish popup
+
+let eidMubarak = document.querySelector(".img1");
+let ramadan = document.querySelector(".img2");
+let div = document.querySelector(".mainBox");
+let OkBtn = document.querySelector(".okay");
+let startEid = new Date("2024-04-09");
+let endEid = new Date("2024-04-11");
+let curentDate = new Date();
+
+
+// wish popup closed
+
+OkBtn.addEventListener("click", () => {
+  div.style.display = "none";
+});
+
+//  common settimeout function 
+let wishshowfunc = setTimeout(() => {
+  div.style.display = "block";
+}, 2000);
+
+// this condition will show img difrent time difrent img 
+
+if (curentDate >= startEid && curentDate <= endEid) {
+  eidMubarak.style.display = "block";
+  ramadan.style.display = "none";
+  wishshowfunc();
+} else if (curentDate < startEid) {
+  wishshowfunc();
+}
+
+// this function clear main popup box 
+function wishes() {
+  // Set the date we're counting down to
+  var countDownDate = new Date(" april 11, 2024 00:00:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.querySelector(".mainBox").innerHTML = " ";
+    }
+  }, 1000);
+
+  return x;
+}
+
+wishes();
